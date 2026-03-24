697c0492bb1340806cfdd210ff1f4826b7333e35
Guntas Singh <guntas7347@gmail.com>
2026-03-24 21:20:57 +0530

    bug fix

--- app/(main)/gallery/page.tsx
+++ app/(main)/gallery/page.tsx
@@ -1,5 +1,4 @@
 import ContactCard from "@/components/ContactCard";
-import { getToken } from "@/lib/firebase/firebaseAdmin";
 import { getGallery } from "@/lib/instagramPosts";
 import { timeAgo } from "@/lib/utils";
 import { Calendar, Loader } from "lucide-react";
@@ -15,10 +14,6 @@ type GalleryImage = {
 
 const GalleryPage = async () => {
   const images: GalleryImage[] = await getGallery();
-
-  const token = await getToken();
-
-  console.log(token);
 
   const hero = images.slice(0, 3);
   const grid = images.slice(0);
